6cbc432a6f8ff024946faa9420db85bd13de93d9
Shiva-Karthik <karthikkonathala6@gmail.com>
2022-05-29 23:19:46 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ function App() {
   const [emptyDivCircle, setEmptyDivCircle] = useState([]);
   const [loading, setLoading] = useState(true);
   const [text, setText] = useState("");
+  console.log('text:', typeof text)
   let obj = {};
   const colours = useRef([]);
-  const generateColor = () =>
-    "#" + Math.floor(Math.random() * 16777215).toString(16);
 
+  {/* Generating random colours */}
+  const generateColor = () => Math.floor(Math.random() * 16777215).toString(16);
+  
+  {/* Mounting unique colour circles */}
   useEffect(() => {
     for (let i = 0; i < 5; i++) {
-      let currentColor = generateColor();
+      let currentColor = "#" + generateColor();
       if (obj[currentColor]) {
         i--;
         continue;
@@ -32,13 +35,20 @@ function App() {
   if (loading) {
     return <div>Loading....</div>;
   }
+
+  {/* Moving circles into empty div */}
   const handleSubmit = (e) => {
+    if(text>5||text<1){
+      return;
+    }
     e.preventDefault();
     let shooted_div = colours.current[text - 1];
     let filter_div = colours.current.filter((el, i) => text - 1 != i);
     colours.current = filter_div;
     setEmptyDivCircle([...emptyDivCircle, shooted_div]);
   };
+
+  {/* Moving circles back to original position*/}
   const handleReverse = (index) => {
     let shooted_div = emptyDivCircle[index];
     let filter_div = emptyDivCircle.filter((el, i) => index != i);
@@ -49,22 +59,34 @@ function App() {
   return (
     <div className="App">
       <div>
-        <div>
-          <form action="" onSubmit={handleSubmit}>
+        {/* Showing number of shooted circles*/}
+        <div><h1>Shooted circles {emptyDivCircle.length}</h1></div>
+        {
+         text>5||text<1 ? "Shoot the circle from 1 to 5" : "" 
+        }
+
+        {/* Input form */}
+        <div className="form" style={{padding:"20px"}}>
+          <form action="" onSubmit={handleSubmit} >
             <input
+            style={{padding:"10px"}}
               type="number"
               onChange={(e) => setText(e.target.value)}
               value={text}
             />
-            <button type="submit">Shoot</button>
+            <button style={{padding:"10px"}} type="submit">Shoot</button>
           </form>
         </div>
+
+        {/* empty div */}
         <div className="empty_div">
           {emptyDivCircle.map((el, i) => (
             <div {...el} onClick={() => handleReverse(i)}></div>
           ))}
         </div>
       </div>
+
+      {/* Random unique colour circles div */}
       <div className="randomCircle">
         {colours.current.map((el) => (
           <div {...el}></div>
